153c713e7db6ddcfe459ecf1a522f2d3417d84a2
R26 <95674004+R26AST@users.noreply.github.com>
2024-02-20 18:17:45 +0700

    Update app2.js

--- CBT/TO-OSK/CBT-pg/Js/Shared/app2.js
+++ CBT/TO-OSK/CBT-pg/Js/Shared/app2.js
@@ -76,7 +76,12 @@ var exam = {
             "answerPosition": 0
         },
         {
-            "question": "Berapakah perbandingan diameter sudut bulan purnama saat <i>perigee</i> dan <i>aphogee</i>?",
+            "question": `A star with a density that is not homogeneous, follows the equation : 
+	    <img src=".jpg" />
+		    Where r is a distance from center to some point of <i>r</i> and <i>R</i> was the radius of 
+      the stars, which is equals to 5 times radius of the sun, while <i>ρ<sub>0</sub></i> = 3700<i>kg/m<sup>3</sup></i>
+      Calculate the accumulated mass at the distance of 1/2 <i>R</i>! (Hint : Use the continuity mass equation! 
+      <img src="conti-mass.jpg" />) `,
             "options": ["0,8", "0,9", "1", "1,1", "1,22"],
             "answerPosition": 3
         },
