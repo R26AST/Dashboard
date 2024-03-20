78d084634e9fcec01f62fdc2b1ad90266c4ada9b
R26 <95674004+R26AST@users.noreply.github.com>
2024-03-20 22:48:02 +0700

    Update script.js

--- Dashboard/script.js
+++ Dashboard/script.js
@@ -117,7 +117,7 @@ var x = setInterval(function() {
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  document.getElementById("ket_OSN_essay").innerHTML = days + "<sup>h</sup> : " + hours + "<sup>j</sup> : " + minutes + "<sup>m</sup> : " + seconds+ "<sup>d</sup>";
+  document.getElementById("nilai_OSN_essay").innerHTML = days + "<sup>h</sup> : " + hours + "<sup>j</sup> : " + minutes + "<sup>m</sup> : " + seconds+ "<sup>d</sup>";
   
   if (distance > 0) {
   document.getElementById("OSN_essay").innerHTML = "Segera";
